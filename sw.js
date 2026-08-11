7358eb1046cc0271ffa85c250203918653b5806d
carvalhogomesag <126914230+carvalhogomesag@users.noreply.github.com>
2026-08-11 22:26:37 +0100

    estavel mas sem enviar relatorio para tela

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * Faz: Controla a cache offline de todos os recursos ativos da aplicação.
  */
 
-const CACHE_NAME = 'classificapack-v73.3';
+const CACHE_NAME = 'classificapack-v73.4';
 
 const ASSETS = [
   './',
